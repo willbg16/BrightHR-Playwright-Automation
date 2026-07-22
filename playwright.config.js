47bb1a45cfaca79cb9f1552ef1ee3479f75c0985
William Beresford-Grindrod <williamberesford-grindrod@Hollies-MacBook-Pro.local>
2026-07-22 16:35:32 +0100

    Increase Playwright expect timeout for CI

--- playwright.config.js
+++ playwright.config.js
@@ -24,6 +24,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
+expect: {
+  timeout: 15000,
+},
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
