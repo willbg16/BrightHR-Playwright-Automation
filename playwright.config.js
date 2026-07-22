2d9973f7c85005c516f9b26a9d8e058077097868
William Beresford-Grindrod <williamberesford-grindrod@Hollies-MacBook-Pro.local>
2026-07-22 16:57:42 +0100

    Increase CI timeout and disable retries

--- playwright.config.js
+++ playwright.config.js
@@ -14,20 +14,26 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+
+  /* Maximum time one test can run */
+  timeout: 60000,
+
   /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+
+  /* Fail the build on CI if you accidentally left test.only */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
+
   retries: 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   reporter: 'html',
 
-expect: {
-  timeout: 60000,
-},
+  expect: {
+    timeout: 60000,
+  },
+
+  use: {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
